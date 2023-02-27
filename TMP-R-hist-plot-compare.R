6ed9a67233387974c51749c29d48bfa5a7f8f039
Mark Baillie <bailliem@gmail.com>
2023-02-27 19:42:09 +0000

    add to sc

--- TMP-R-hist-plot-compare.R
+++ TMP-R-hist-plot-compare.R
@@ -18,6 +18,15 @@ dat <- dat |>
   tidyr::drop_na(AVAL) |>
   mutate(AVAL2 = pseudo_log(AVAL, sigma))
 
+dat_median <- median(dat$AVAL, na.rm = TRUE)
+dat_5 <- fivenum(dat$AVAL, na.rm = TRUE)
+
+
+## expand range 
+rng_vals <- scales::expand_range(range(dat$AVAL, na.rm = TRUE), mul = 0.01)
+
+## range of the data 
+rng <- range(dat$AVAL)
 
 a <- 
   dat |> 
@@ -27,12 +36,33 @@ a <-
     bins = n_bars, 
     #breaks = brks,
     center = 0,
-    alpha = 0.6,
+    alpha = 0.4,
     fill = "firebrick2") +
   scale_x_continuous(
     limit = c(min(dat$AVAL), max(dat$AVAL)),
     breaks = fivenum(dat$AVAL),
-    labels = round(fivenum(dat$AVAL),1))
+    labels = round(fivenum(dat$AVAL),1)) +
+  geom_point(data = NULL, aes(x = rng_vals[1], y = 1), color = "transparent", size = 0.1) +
+  geom_point(data = NULL, aes(x = rng_vals[2], y = 1), color = "transparent", size = 0.1) +
+  scale_y_continuous(expand = c(0, 0)) +
+  geom_vline(xintercept = dat_5[1], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[2], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[3], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[4], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[5], colour = "red", alpha = 0.2) +
+  #theme_void() +
+  theme(
+    axis.text.x = element_text(
+      color = "black",
+      vjust = -2,
+      size = 10
+    ),
+    axis.line.x = element_line(color = "black"),
+    axis.ticks.x = element_line(color = "black"),
+    axis.ticks.length.x = unit(1, "mm"),
+    plot.margin = margin(1, 1, 3, 1)
+    #text = element_text(family = "mono", size = 10)
+  )
 
 x <- dat$AVAL2
 
@@ -57,7 +87,29 @@ b <-
   scale_x_continuous(
     limit = c(min(dat$AVAL2), max(dat$AVAL2)),
     breaks = fivenum(dat$AVAL2),
-    labels = round(fivenum(dat$AVAL),1))
+    labels = round(fivenum(dat$AVAL),1)) +
+  geom_point(data = NULL, aes(x = rng_vals[1], y = 1), color = "transparent", size = 0.1) +
+  geom_point(data = NULL, aes(x = rng_vals[2], y = 1), color = "transparent", size = 0.1) +
+  scale_y_continuous(expand = c(0, 0)) +
+  geom_vline(xintercept = dat_5[1], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[2], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[3], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[4], colour = "red", alpha = 0.2) +
+  geom_vline(xintercept = dat_5[5], colour = "red", alpha = 0.2) +
+  #theme_void() +
+  theme(
+    axis.text.x = element_text(
+      color = "black",
+      vjust = -2,
+      size = 10
+    ),
+    axis.line.x = element_line(color = "black"),
+    axis.ticks.x = element_line(color = "black"),
+    axis.ticks.length.x = unit(1, "mm"),
+    plot.margin = margin(1, 1, 3, 1)
+    #text = element_text(family = "mono", size = 10)
+  )
+
 
 plt <- a + b
 
