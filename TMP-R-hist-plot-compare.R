c08a69854b73ebf091fb1715fa48f57da3e3d5a2
Mark Baillie <bailliem@gmail.com>
2023-02-27 19:21:26 +0000

    add to sc

---
+++ TMP-R-hist-plot-compare.R
@@ -0,0 +1,66 @@
+pseudo_log <- function(x, sigma = 1, base = 10){
+  return(asinh(x / (2 * sigma)) / log(base))
+}
+
+
+
+compare_dist_plot <- function(dat, n_bars = 100, bin_width = NULL, sigma = NULL){
+  
+
+
+bin_width <- diff(range(dat$AVAL,na.rm=T))/min(length(unique(dat$AVAL)),100)
+sigma = 0.041068411 
+#sigma = 2.041040980 
+n_bars <- 100
+
+
+dat <- dat |>
+  tidyr::drop_na(AVAL) |>
+  mutate(AVAL2 = pseudo_log(AVAL, sigma))
+
+
+a <- 
+  dat |> 
+  ggplot(aes(x = AVAL, y=..density..)) +
+  geom_histogram(
+    binwidth = bin_width, 
+    bins = n_bars, 
+    #breaks = brks,
+    center = 0,
+    alpha = 0.6,
+    fill = "firebrick2") +
+  scale_x_continuous(
+    limit = c(min(dat$AVAL), max(dat$AVAL)),
+    breaks = fivenum(dat$AVAL),
+    labels = round(fivenum(dat$AVAL),1))
+
+x <- dat$AVAL2
+
+if(length(unique(x))<n_bars*2) {
+  brks <- unique(x) 
+}  else {
+  brks1 <- seq(min(x), max(x), length.out=n_bars+1)
+  brks2 <- quantile(x, seq(0,1,1/n_bars))
+  brks <- (brks2 + 3*brks1)/4
+}
+
+b <- 
+  dat |> 
+  ggplot(aes(x = AVAL2, y=..density..)) +
+  geom_histogram(
+    #bin_width = bin_width, 
+    #n_bars = n_bars, 
+    breaks = brks,
+                 center = 0,
+                 alpha = 0.6,
+                 fill = "firebrick2") +
+  scale_x_continuous(
+    limit = c(min(dat$AVAL2), max(dat$AVAL2)),
+    breaks = fivenum(dat$AVAL2),
+    labels = round(fivenum(dat$AVAL),1))
+
+plt <- a + b
+
+return(plt)
+}
+
