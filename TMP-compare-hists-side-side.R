76635d79d9aef4463236c444989fadb5cdbe8960
Mark Baillie <bailliem@gmail.com>
2023-03-08 21:09:21 +0100

    add to sc

--- TMP-compare-hists-side-side.R
+++ TMP-compare-hists-side-side.R
@@ -14,31 +14,41 @@ rm(a, ADLB, ADLB_02, ADSL, param, suggested_transforms, new_vars)
 
 trans_plot <- 
   dat |>
-  ggplot(aes(x = AVAL, after_stat(density))) +
-  geom_histogram(
-    bins = 200,
-    center = 0,
-    alpha = 0.6,
-    fill = "firebrick2"
-  ) +
+  ggplot(aes(x = AVAL)) + #, after_stat(density))) +
+  
   scale_x_continuous(
     breaks = fivenum(dat$AVAL, na.rm = TRUE),
     labels = round(fivenum(dat$AVAL02, na.rm = TRUE), 1),
     guide = guide_axis(check.overlap = TRUE)
   ) +
-  scale_y_continuous(expand = c(0, 0)) +
-  theme_void(base_size = 15) +
+
+  # scale_y_continuous(expand = c(0, 0)) +
+  geom_rug(sides = "b", alpha = 0.2, color = "black", outside = FALSE, length = unit(2.5, "mm")) +
+  
+  geom_histogram(#color = "firebrick2",
+    fill = "firebrick2",
+    bins = 200,
+    alpha = 0.8) +
+  
+  geom_hline(yintercept = 0, alpha = 0.5) +
+  
+  # theme_void(base_size = 15) +
+  theme_minimal(base_size = 10) +
   theme(
-    axis.text.x = element_text(
-      color = "black",
-      size = 13
-    ),
-    axis.line.x = element_line(color = "black"),
-    axis.ticks.x = element_line(color = "black"),
-    axis.ticks.length.x = unit(1, "mm"),
+    axis.text.y = element_blank(),
+    #axis.text.x = element_text(
+    #  color = "black",
+      #      vjust = -2,
+     # size = 13
+    #),
+    #   axis.line.x = element_line(color = "black"),
+    #   axis.ticks.x = element_line(color = "black"),
+    #    axis.ticks.length.x = unit(1, "mm"),
     axis.title = element_blank(),
     panel.grid.major.x = element_line(color = "grey", linewidth = 0.5),
-    plot.margin = margin(1, 1, 3, 1),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    #  plot.margin = margin(1, 1, 3, 1),
     text = element_text(
       size = 13)
   )
@@ -57,7 +67,7 @@ trans_plot <-
 
 original_plot <- 
   dat |>
-  ggplot(aes(x = AVAL02, after_stat(density))) +
+  ggplot(aes(x = AVAL02)) + #, after_stat(density))) +
   
   scale_x_continuous(
     breaks = fivenum(dat$AVAL02),
@@ -65,33 +75,47 @@ original_plot <-
     guide = guide_axis(check.overlap = TRUE)
   ) +
   
-  scale_y_continuous(expand = c(0, 0)) +
+ # scale_y_continuous(expand = c(0, 0)) +
+  geom_rug(sides = "b", alpha = 0.2, color = "black", outside = FALSE, length = unit(2.5, "mm")) +
 
   geom_histogram(#color = "firebrick2",
     fill = "firebrick2",
     bins = 200,
     alpha = 0.8) +
   
-  theme_void(base_size = 15) +
+  geom_hline(yintercept = 0, alpha = 0.5) +
+  
+  # theme_void(base_size = 15) +
+  theme_minimal() +
   theme(
-    axis.text.x = element_text(
-      color = "black",
-      #      vjust = -2,
-      size = 13
-    ),
-    axis.line.x = element_line(color = "black"),
-    axis.ticks.x = element_line(color = "black"),
-    axis.ticks.length.x = unit(1, "mm"),
+    axis.text.y = element_blank(),
+    # axis.text.x = element_text(
+    #   color = "black",
+    #   #      vjust = -2,
+    #   size = 13
+    # ),
+ #   axis.line.x = element_line(color = "black"),
+ #   axis.ticks.x = element_line(color = "black"),
+#    axis.ticks.length.x = unit(1, "mm"),
     axis.title = element_blank(),
     panel.grid.major.x = element_line(color = "grey", linewidth = 0.5),
-    plot.margin = margin(1, 1, 3, 1),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+  #  plot.margin = margin(1, 1, 3, 1),
     text = element_text(
       size = 13)
   )
 
 
-original_plot / trans_plot
+original_plot + trans_plot
 
-
-
-
+# dat |>
+#   ggplot(aes(x = AVAL02)) +
+#   geom_histogram(#color = "firebrick2",
+#     fill = "firebrick2",
+#     bins = 200,
+#     alpha = 0.8) +
+# #  scale_y_continuous(expand = c(0, 0)) +  
+#   theme_minimal() +
+#   geom_rug(sides = "b", alpha = 0.2, color = "black", outside = FALSE, length = unit(2.5, "mm")) 
+# 
