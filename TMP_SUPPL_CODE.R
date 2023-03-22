4f12a99b62e8cc8ffb0bd9ac5090e548329c7abd
bailliem <bailliem@gmail.com>
2023-03-22 09:26:48 +0100

    wrap up suppl

--- TMP_SUPPL_CODE.R
+++ TMP_SUPPL_CODE.R
@@ -1,126 +1,69 @@
+set.seed(1972)
+m_pct <- .5
 
-## assumes the same number of variables but with transformation
-key_predictors <- key_predictors |> stringr::str_sort()
-key_predictors_02 <- key_predictors_02 |> stringr::str_sort()
 
-# medians of all key predictors, selected variables will be adjusted to these medians in effect plots
-model_df_medians_02 <- model_data_02 |>
-  select(-USUBJID, -BACTEREMIAN) |>
-  summarise_all(median)
+sel_central <- (model_df_complete$AGE > quantile(model_df_complete$AGE, 0.5-m_pct/2)) & 
+  (model_df_complete$AGE < quantile(model_df_complete$AGE, 0.5+m_pct/2)) #needed later
 
-lookup <- c(WBC_noNEU_T = "WBC_noNEU", CREA_T = "CREA")
-lookup02 <- c(WBC_noNEU = "WBC_noNEU_T", CREA = "CREA_T")
 
-for(predictor in key_predictors){
-  
-  if(!(predictor %in% key_predictors_02))
-  {
-    print(predictor)
-    
-    predictor_02 <- str_remove(predictor, "_T")
-    
-    ## make a function 
-    
-    ## bind model data with median of current predictor
-    new_data <- bind_cols(
-      model_data |> select(USUBJID, all_of(predictor)), 
-      model_df_medians |> select(-predictor)) 
-    
-    
-    ## bind model data with median of current predictor
-    new_data02 <- bind_cols(
-      model_data_02 |> select(USUBJID, all_of(predictor_02)), 
-      model_df_medians_02 |> select(-predictor_02)) 
-  
-    new_data1 <- new_data |>
-      left_join(new_data02 |> select(USUBJID, all_of(predictor_02)), by = "USUBJID") |>
-      select(-USUBJID) |>
-      distinct()
-  
-    new_data02_1 <- new_data02 |>     
-      left_join(new_data |> select(USUBJID, all_of(predictor)), by = "USUBJID") |>
-      select(-USUBJID) |>
-      distinct()
-    
-      
-    
-    # predict on transformed data
-    pred_trans <- predict(fit_mfp_complete, newdata = new_data1 , type = 'link', se.fit = TRUE)    
-    
-    pred_original <- predict(fit_mfp_complete_02, newdata = new_data02_1 , type = 'link', se.fit = TRUE)
-    
-    
-    plot_df_a <- bind_cols(
-      new_data1,
-      yhat = pred_original$fit,
-      yhat.lwr = pred_original$fit - 1.96*pred_original$se.fit,      
-      yhat.upr = pred_original$fit + 1.96*pred_original$se.fit) |>
-      mutate(model = "pseudo-log transformed")
-    
-    plot_df_b <- bind_cols(
-      new_data02_1,
-      yhat = pred_trans$fit,
-      yhat.lwr = pred_trans$fit - 1.96*pred_trans$se.fit,
-      yhat.upr = pred_trans$fit + 1.96*pred_trans$se.fit) |>
-      mutate(model = "original scale")
-    
-    plot_df <- bind_rows(plot_df_a, plot_df_b)
-    
-    
-    
-    p_trans <- 
-      plot_df |>
-      ggplot(aes(x = !!sym(predictor), y = yhat, ymin = yhat.lwr, ymax = yhat.upr, color = model, fill = model)) +
-      geom_ribbon(alpha = .2, color = NA) +
-      geom_line() +
-      geom_rug(sides="b") +
-      labs(
-        y = 'log odds',
-        title = 'on pseudo-log scale',
-        x = predictor,
-        color = 'model with data on',
-        fill = 'model with data on'
-      ) +
-      theme_minimal() +
-      scale_color_ptol() +
-      scale_fill_ptol()
-    
-    
-    p_original <- 
-      plot_df |>
-      ggplot(aes(x = !!sym(predictor_02), y = yhat, ymin = yhat.lwr, ymax = yhat.upr, color = model, fill = model)) +
-      geom_ribbon(alpha = .2, color = NA) +
-      geom_line() +
-      geom_rug(sides="b") +
-      labs(
-        y = 'log odds',
-        title = 'on original scale',
-        x = predictor_02,
-        color = 'model with data on',
-        fill = 'model with data on'
-      ) +
-      theme_minimal() +
-      scale_color_ptol() +
-      scale_fill_ptol()
-    
-    
-    
-    p <- p_original + (p_trans +
-                         theme(
-                           axis.title.y = element_text(color = NA)
-                         )) +
-      plot_layout(guides = 'collect') +
-      plot_annotation(caption = 'adjusted to medians of all other covariates')  & 
-      theme(legend.position = 'bottom')
-    
-    print(p)
-    
-    
-    
-    
-  }
+sel_sample <- as.logical(round(runif(dim(model_df_complete)[1]))) # 50% random selection, needed later
+
+pred_complete <- predict(fit_mfp_complete, 
+                         newdata = model_df_trans_complete,
+                         type = 'response')
+
+y_complete <- fit_mfp_complete$y
+
+pred_central <- predict(fit_mfp_complete, 
+                        newdata = model_df_trans_complete[sel_central,],
+                        type = 'response')
+
+y_central <- fit_mfp_complete$y[sel_central]
+
+pred_sample <- predict(fit_mfp_complete, 
+                       newdata = model_df_trans_complete[sel_sample,],
+                       type = 'response')
+
+y_sample <- fit_mfp_complete$y[sel_sample]
+
+
+r_squared_efron <- function(y, prediction){
+  n <- length(y)
+  1-(((1/n)*sum((y-prediction)^2))/((1/n)*sum((y-mean(y))^2)))
 }
 
-    
-    
-      
+tribble(
+  ~data, ~AUC, ~`scaled Brier score`,
+  'complete', auc(y_complete, pred_complete) |> as.numeric(), cor(y_complete, pred_complete)^2,
+  'central 50%', auc(y_central, pred_central) |> as.numeric(), cor(y_central, pred_central)^2,
+  '50% sample', auc(y_sample, pred_sample) |> as.numeric(), cor(y_sample, pred_sample)^2,
+) |>
+  gt() %>%
+  fmt_number(2, decimals = 3) |>
+  fmt_number(3, decimals = 5) |>
+  gt_theme_538()
+
+p_ex2 <- rbind(
+  tibble(
+    BC = y_complete,
+    prediction = pred_complete,
+    model = 'complete data'
+  ),
+  tibble(
+    BC = y_central,
+    prediction = pred_central,
+    model = 'within IQR (age)'
+  ),
+  tibble(
+    BC = y_sample,
+    prediction = pred_sample,
+    model = 'random 50% subsample'
+  )) |>
+  mutate(model = factor(model, levels = c('complete data', 'within IQR (age)', 'random 50% subsample'))) %>%
+  ggplot(aes(x = factor(BC), y = prediction, group = BC)) +
+  geom_boxplot() + 
+  facet_grid(~model) +
+  theme_minimal() +
+  labs(x = 'BC')
+
+p_ex2
